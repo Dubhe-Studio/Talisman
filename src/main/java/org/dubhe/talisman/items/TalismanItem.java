cda3f2a8a0c846332ec7ae887dca29c4de30f0be
Cjsah <2684117397@qq.com>
2021-06-07 15:44:01 +0800

    update

--- src/main/java/org/dubhe/talisman/items/TalismanItem.java
+++ src/main/java/org/dubhe/talisman/items/TalismanItem.java
@@ -1,7 +1,6 @@
 package org.dubhe.talisman.items;
 
 import net.minecraft.client.util.ITooltipFlag;
-import net.minecraft.entity.LivingEntity;
 import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemStack;
@@ -9,19 +8,16 @@ import net.minecraft.item.Rarity;
 import net.minecraft.nbt.CompoundNBT;
 import net.minecraft.nbt.INBT;
 import net.minecraft.nbt.ListNBT;
-import net.minecraft.nbt.StringNBT;
+import net.minecraft.stats.Stats;
+import net.minecraft.util.ActionResult;
+import net.minecraft.util.Hand;
 import net.minecraft.util.text.ITextComponent;
-import net.minecraft.util.text.Style;
 import net.minecraft.util.text.TextFormatting;
 import net.minecraft.util.text.TranslationTextComponent;
 import net.minecraft.world.World;
-import org.dubhe.talisman.ModInitializer;
-import org.dubhe.talisman.TalismanUtils;
 import org.dubhe.talisman.entities.TalismanEntity;
 
 import javax.annotation.Nullable;
-import java.util.Collection;
-import java.util.Collections;
 import java.util.List;
 
 @SuppressWarnings("NullableProblems")
@@ -29,10 +25,6 @@ public class TalismanItem extends Item implements WithDefaultNbt {
 
     public TalismanItem(Properties properties) {
         super(properties.rarity(Rarity.UNCOMMON));
-    }
-
-    public TalismanEntity createEntity(World world, PlayerEntity player, ItemStack stack) {
-        return new TalismanEntity(world, player.getPositionVec(), player, TalismanUtils.toCollection(stack.getOrCreateTag().getList("executes", 8)));
     }
 
     @Override
@@ -58,16 +50,22 @@ public class TalismanItem extends Item implements WithDefaultNbt {
     }
 
     @Override
-    public void onUse(World world, LivingEntity entity, ItemStack stack, int count) {
-        boolean throwable = stack.getOrCreateTag().getBoolean("throwable");
-        int needExperiences = stack.getOrCreateTag().getInt("needExperiences");
-        Collection<String> executes = TalismanUtils.toCollection(stack.getOrCreateTag().getList("executes", 8));
-
-
-
-
-
-        // 未完待续。。。困了。。。
-
+    public ActionResult<ItemStack> onItemRightClick(World world, PlayerEntity player, Hand hand) {
+        ItemStack item = player.getHeldItem(hand);
+        boolean throwable = item.getOrCreateTag().getBoolean("throwable");
+        int needExperiences = item.getOrCreateTag().getInt("needExperiences");
+        if (player.isCreative() || player.experienceTotal >= needExperiences) {
+            TalismanEntity talisman = this.createEntity(world, player, item, throwable);
+            world.addEntity(talisman);
+            if (!player.isCreative()) item.shrink(1);
+            player.addStat(Stats.ITEM_USED.get(this));
+            return ActionResult.resultSuccess(item);
+        }
+        return ActionResult.resultFail(item);
     }
+
+    public TalismanEntity createEntity(World world, PlayerEntity player, ItemStack stack, boolean throwable) {
+        return new TalismanEntity(world, player.getPositionVec(), player, stack.getOrCreateTag().getList("executes", 8), throwable);
+    }
+
 }
