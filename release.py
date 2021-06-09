77b101383efb8253ebc2ef79e52bcbccb3fbc471
Cjsah <2684117397@qq.com>
2021-06-09 21:22:27 +0800

    update

--- release.py
+++ release.py
@@ -1,13 +1,13 @@
 # -*- coding: utf-8 -*-
 # by Cjsah
 
-import os, re, sys
-
-args = sys.argv[1:]
+import os, re
 
 files = [file for file in os.listdir(os.path.join(os.getcwd(), "build", "libs")) if re.match('[\s\S]+-[0-9]+\.[0-9]+\.[0-9]+.jar', file)]
 
+tag = files[0].split("-")[1][:-4]
+
 if len(files) > 0:
-    os.system('gh release create {a} --title "{a}" --notes {b}'.format(a=args[0], b=args[1]))
+    os.system('gh release create {a} --title "{a}" --notes ""'.format(a=tag))
 for file in files:
-    os.system("gh release upload {} build/libs/{}".format(args[0], file))
+    os.system("gh release upload {} build/libs/{}".format(tag, file))
