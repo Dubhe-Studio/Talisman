8ebed507476ca85ae33c1081ae1424f4973c6e83
Ely_Official <shi_yichen@hotmail.com>
2021-06-09 22:30:25 +0800

    Update release.py

--- release.py
+++ release.py
@@ -8,5 +8,7 @@ files = [file for file in os.listdir(os.path.join(os.getcwd(), "build", "libs"))
 if len(files) > 0:
     file = files[-1]
     tag = file.split("-")[1][:-4]
-    print os.system('gh release create {a} --title "{a}" --notes ""'.format(a=tag))
-    print os.system("gh release upload {} build/libs/{}".format(tag, file))
+    p1=os.popen('gh release create {a} --title "{a}" --notes ""'.format(a=tag))
+    print p1.read()
+    p2=os.popen("gh release upload {} build/libs/{}".format(tag, file))
+    print p2.read()
