63d4d00d514338b0ce6851ca93e1fcd8ca682e62
Ely_Official <shi_yichen@hotmail.com>
2021-06-10 00:51:50 +0800

    Update release.py

--- release.py
+++ release.py
@@ -1,4 +1,4 @@
-# -*- coding: Unicode -*-
+# -*- coding: UTF-8 -*-
 import os
 import re
 files = [file for file in os.listdir(os.path.join(os.getcwd(), "build", "libs")) if re.match('[\s\S]+-[0-9]+\.[0-9]+\.[0-9]+.jar', file)]
@@ -6,5 +6,5 @@ files = [file for file in os.listdir(os.path.join(os.getcwd(), "build", "libs"))
 if len(files) > 0:
     file = files[-1]
     tag = file.split("-")[1][:-4]
-    os.popen('C:\Users\TeamCity\AppData\Local\Programs\Python\Python38\python.exe release create {a} --title "{a}" --notes "" -R Dubhe-Studio/Talisman'.format(a=tag))
-    os.popen("C:\Users\TeamCity\AppData\Local\Programs\Python\Python38\python.exe release upload {} build/libs/{} -R Dubhe-Studio/Talisman".format(tag, file))
+    os.popen(u'C:\Users\TeamCity\AppData\Local\Programs\Python\Python38\python.exe release create {a} --title "{a}" --notes "" -R Dubhe-Studio/Talisman'.format(a=tag))
+    os.popen(u"C:\Users\TeamCity\AppData\Local\Programs\Python\Python38\python.exe release upload {} build/libs/{} -R Dubhe-Studio/Talisman".format(tag, file))
