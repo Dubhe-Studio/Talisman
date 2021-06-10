c2f17fc7c5abb6296d9729634c20132134441e36
Cjsah <2684117397@qq.com>
2021-06-10 17:15:46 +0800

    update

--- release.py
+++ release.py
@@ -1,12 +1,24 @@
 # -*- coding: utf-8 -*-
 # by Cjsah
 
-import os, re
+import os, re, requests
+from lxml import etree
 
-files = [file for file in os.listdir(os.path.join(os.getcwd(), "build", "libs")) if re.match('[\s\S]+-[0-9]+\.[0-9]+\.[0-9]+.jar', file)]
 
-if len(files) > 0:
-    file = files[-1]
-    tag = file.split("-")[1][:-4]
-    os.system('""C:\Program Files (x86)\GitHub CLI\gh" release create {a} --title "{a}" --notes ""'.format(a=tag))
-    os.system('"C:\Program Files (x86)\GitHub CLI\gh" release upload {} {}/build/libs/{}'.format(tag, os.getcwd().replace("\\", "/"), file))
+def rename(name, num):
+    file = os.path.join(os.getcwd(), 'build', 'libs')
+    result = '{}+build.{}.jar'.format(name[:-4], num)
+    os.rename(os.path.join(file, name), os.path.join(file, result))
+    return result
+
+
+if __name__ == '__main__':
+    files = [file for file in os.listdir(os.path.join(os.getcwd(), 'build', 'libs')) if re.match('[\s\S]+-[0-9]+\.[0-9]+\.[0-9]+.jar', file)]
+
+    if len(files) > 0:
+        xml = requests.get('http://tcdev.elyimg.pro/guestAuth/app/rest/builds').content
+        number = etree.XML(xml).xpath('//builds/build/@number')[0]
+        file = rename(files[-1], number)
+        tag = re.split('[-+]', file)[-2]
+        os.system('""C:\Program Files (x86)\GitHub CLI\gh" release create {a} --title "{a}" --notes ""'.format(a=tag))
+        os.system('"C:\Program Files (x86)\GitHub CLI\gh" release upload {} {}/build/libs/{}'.format(tag, os.getcwd().replace("\\", "/"), file))
