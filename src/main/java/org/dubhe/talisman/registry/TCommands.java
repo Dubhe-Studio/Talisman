f0d4f0a0a6cb31a514f0edd9bd154b9bde3b1646
Gu_ZT <administration@ztxy666.cn>
2021-07-26 23:16:14 +0800

    Update TCommands.java

--- src/main/java/org/dubhe/talisman/registry/TCommands.java
+++ src/main/java/org/dubhe/talisman/registry/TCommands.java
@@ -31,8 +31,15 @@ public class TCommands {
         TYPES.put("sin", context -> Math.sin(DoubleArgumentType.getDouble(context, "value")) * DoubleArgumentType.getDouble(context, "scale"));
         TYPES.put("cos", context -> Math.cos(DoubleArgumentType.getDouble(context, "value")) * DoubleArgumentType.getDouble(context, "scale"));
         TYPES.put("tan", context -> Math.tan(DoubleArgumentType.getDouble(context, "value")) * DoubleArgumentType.getDouble(context, "scale"));
-
-
+        TYPES.put("cot", context -> 1/Math.tan(DoubleArgumentType.getDouble(context, "value")) * DoubleArgumentType.getDouble(context, "scale"));
+        TYPES.put("asin", context -> Math.asin(DoubleArgumentType.getDouble(context, "value")) * DoubleArgumentType.getDouble(context, "scale"));
+        TYPES.put("acos", context -> Math.acos(DoubleArgumentType.getDouble(context, "value")) * DoubleArgumentType.getDouble(context, "scale"));
+        TYPES.put("atan", context -> Math.atan(DoubleArgumentType.getDouble(context, "value")) * DoubleArgumentType.getDouble(context, "scale"));
+        TYPES.put("acot", context -> 1/Math.atan(DoubleArgumentType.getDouble(context, "value")) * DoubleArgumentType.getDouble(context, "scale"));
+        TYPES.put("sqrt", context -> Math.sqrt(DoubleArgumentType.getDouble(context, "value")) * DoubleArgumentType.getDouble(context, "scale"));
+        TYPES.put("rsqrt", context -> 1/Math.sqrt(DoubleArgumentType.getDouble(context, "value")) * DoubleArgumentType.getDouble(context, "scale"));
+        TYPES.put("abs", context -> Math.abs(DoubleArgumentType.getDouble(context, "value")) * DoubleArgumentType.getDouble(context, "scale"));
+        TYPES.put("ln", context -> Math.log(DoubleArgumentType.getDouble(context, "value")) * DoubleArgumentType.getDouble(context, "scale"));
+        TYPES.put("lg", context -> Math.log10(DoubleArgumentType.getDouble(context, "value")) * DoubleArgumentType.getDouble(context, "scale"));
     }
-
 }
