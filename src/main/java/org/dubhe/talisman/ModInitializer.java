8cce69c2abe8a57ef3c47400ac2576df7bb04a8a
Cjsah <2684117397@qq.com>
2021-07-31 22:14:47 +0800

    update

--- src/main/java/org/dubhe/talisman/ModInitializer.java
+++ src/main/java/org/dubhe/talisman/ModInitializer.java
@@ -11,8 +11,6 @@ import net.minecraftforge.common.MinecraftForge;
 import net.minecraftforge.eventbus.api.IEventBus;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
-import org.apache.logging.log4j.LogManager;
-import org.apache.logging.log4j.Logger;
 import org.dubhe.talisman.registry.TBlocks;
 import org.dubhe.talisman.registry.TCommands;
 import org.dubhe.talisman.registry.TContainerTypes;
